53df4c526823e4458747254df336c6d0b17d5910
unknown <jcbonke@yahoo.com.br>
2024-09-28 04:37:25 -0300

    ajustes validators

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Injectable } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatDividerModule} from '@angular/material/divider';
@@ -8,6 +8,7 @@ import { UsuarioFilter } from '../usuario/usuario.model';
 import { HttpClient } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder, FormGroup, Validators,FormControl  } from '@angular/forms';
+import {MatSnackBar,MatSnackBarHorizontalPosition,MatSnackBarVerticalPosition,} from '@angular/material/snack-bar';
 
  apiService : ApiService
 @Component({
@@ -18,26 +19,57 @@ import { FormBuilder, FormGroup, Validators,FormControl  } from '@angular/forms'
 
 })
 export class LoginComponent{
-  constructor(public apiService : ApiService,public  http : HttpClient, public formBuilder: FormBuilder){}
+  constructor(public apiService : ApiService,public  http : HttpClient, public formBuilder: FormBuilder){
+  }
+
+  private _snackBar = inject(MatSnackBar);
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
   formulario: FormGroup
   senha : string = ''
   email : string = ""
-  ngOnInit(){
-    this.formulario = this.formBuilder.group({
-      email:[this.email,Validators.required],
-      senha:[this.senha,Validators.minLength(6)]
-    })
+
+ async ngOnInit() : Promise<void>{
+  this.formulario = new FormGroup({
+    email: new FormControl(this.email, [
+      Validators.required,
+      Validators.email
+    ]),
+    senha: new FormControl(this.senha, [
+      Validators.required,
+      Validators.minLength(6)
+    ])
+  });
+
+
   }
 
 
 
 
   logar(){
-    let usuarioFilter: UsuarioFilter = new UsuarioFilter();
+    let usuarioFilter: UsuarioFilter = new UsuarioFilter()
+    usuarioFilter.email = this.formulario.value['email']
+    usuarioFilter.senha = this.formulario.value['senha']
 
-    usuarioFilter.email = this.formulario.value['email'];
-    usuarioFilter.senha = this.formulario.value['senha'];
-    alert(usuarioFilter.email)
+    if(!usuarioFilter.email.includes("@")){
+      // const snackBarRef = this._snackBar.open(`'Digite Um e-mail Válido ${usuarioFilter.email}` , '', {
+      //   duration: 2000,
+      //   horizontalPosition: 'center',
+      //   verticalPosition: 'top',
+      // });
+
+
+      // setTimeout(() => {
+      //   snackBarRef.dismiss();
+      // }, 2000);
+    }
+
+
+
+
+
+
   }
 
 }
