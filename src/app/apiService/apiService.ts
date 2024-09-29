e49ada9be3a89cc19d7ba36e8201b677c7dcd5f5
Josebonke <jcbonke@yahoo.com.br>
2024-09-29 20:28:01 -0300

    front  recebendo response do backend

--- src/app/apiService/apiService.ts
+++ src/app/apiService/apiService.ts
@@ -1,49 +1,23 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Subject } from "rxjs/internal/Subject";
+import { inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { UsuarioGRID,UsuarioFilter } from "../usuario/usuario.model";
-import { bootstrapApplication } from '@angular/platform-browser';
-
+import {MatSnackBar,MatSnackBarHorizontalPosition,MatSnackBarVerticalPosition,} from '@angular/material/snack-bar';
 @Injectable({
   providedIn: 'root'
 })
 
 export class ApiService {
-  apiUrl: string = 'https://localhost:7232'
+  apiUrl: string = 'https://localhost:7232/login'
 
+  constructor(private http: HttpClient) { }
 
-  constructor(public http: HttpClient) {}
-  changeItemSubject = new Subject<number>();
-
-
-
-  get usuarios_subscribeCat(){
-    return this.changeItemSubject.asObservable()
-  }
-
-  Logar( parameter: UsuarioFilter = new UsuarioFilter()): Promise<UsuarioGRID[]> {
-
-    const url = `${this.apiUrl}/Login`;
-    let params = new HttpParams();
-
-    Object.entries(parameter).forEach(([key, value]) => {
-      if (value !== undefined && value !== null) {
-        params = params.set(key, value.toString());
-      }
-
-    });
-
-    return new Promise<UsuarioGRID[]>((resolve, reject) => {
-      const body = { ...params };
-      this.http.post<UsuarioGRID[]>(url, parameter)
-          .subscribe({
-              next: (data: UsuarioGRID[]) => {
-                  resolve(data);
-              },
-              error: (error: any) => {
-                  reject(error);
-              }
-          });
-  });
+  login(model: UsuarioFilter): Observable<UsuarioGRID> {
+    return this.http.post<UsuarioGRID>(this.apiUrl, model)
+    .pipe(
+      map(response => response)
+    )
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,18 @@
 import { Component,inject } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule } from '@angular/material/input';
+import {MatFormFieldModule } from '@angular/material/form-field';
+import {MatDividerModule } from '@angular/material/divider';
+import {MatButtonModule } from '@angular/material/button';
 import { ApiService } from '../apiService/apiService';
 import { UsuarioFilter } from '../usuario/usuario.model';
-import { HttpClient } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import {FormGroup,Validators,FormControl} from '@angular/forms';
-import {MatSnackBar,MatSnackBarHorizontalPosition,MatSnackBarVerticalPosition,} from '@angular/material/snack-bar';
+import { FormGroup,Validators,FormControl} from '@angular/forms';
+import { MatSnackBar,MatSnackBarHorizontalPosition,MatSnackBarVerticalPosition,} from '@angular/material/snack-bar';
 import { CommonModule } from '@angular/common';
 import { BaseComponentFormComponent } from '../shared/base-component-form/base-component-form.component';
- apiService : ApiService
+import { tap,catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+apiService : ApiService
 @Component({
   selector:'login-app',
   standalone: true,
@@ -50,8 +51,30 @@ getClassCssbtnLogar():string{
     let usuarioFilter: UsuarioFilter = new UsuarioFilter()
     usuarioFilter.email = this.formulario.value['email']
     usuarioFilter.senha = this.formulario.value['senha']
-    this.apiService.Logar(usuarioFilter)
-    this.setCookie()
+
+    this.apiService.login(usuarioFilter).subscribe({
+      next: (response) => {
+        const token = response;
+        this.cookieService.setCookie('jwtToken', '',7 ); // Salva o token no localStorage
+        console.log('Login realizado com sucesso!', token)
+      },
+      error: (reject) => {
+        const snackBarRef = this._snackBar.open(`Erro  ${reject.error}` , '', {
+        duration: 2000,
+        horizontalPosition: 'center',
+        verticalPosition: 'top',
+        })
+        setTimeout(() => {
+          snackBarRef.dismiss();
+        }, 2000);
+
+
+      }
+    });
+
+
+
+
   }
 
 }
